cb488b973dd8b145c904bb91bf0ff0ec29f29a4e
SUDHAMAI16 <22b81a12b8@cvr.ac.in>
2025-01-11 11:51:31 +0530

    changes updated

---
+++ modeg.js
@@ -0,0 +1,5 @@
+import p,{add,sub} from './mathmos.js'
+console.log(add(2,3))
+console.log(p(15,30))
+console.log(sub(35,55))
+console.log(add(53,60))
---
+++ module1.js
@@ -0,0 +1,11 @@
+function add(a,b){
+    return a+b;
+}
+
+function sub(c,d){
+    return c-d;
+}
+let multiply = (e,f)=>e*f;
+export{add,sub}
+
+export default multiply;
